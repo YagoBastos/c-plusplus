ddca1360c6fd46744adcaf197c336fd6ff0e6530
YagoBastos <Yagobastos02102004@gmail.com>
2022-02-23 15:26:16 -0300

    atualização

--- progs/prog conversao binario para dec/prog conversao.cpp
+++ progs/prog conversao binario para dec/prog conversao.cpp
@@ -1,19 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-    int bin[8];
+    char bin[8];
     int base[8] = {128,64,32,16,8,4,2,1};
     int dec=0;
     printf(" Digite um numero base 2: ");
+    gets(bin);
     for(int i=0;i<=7;i++){
-        scanf("%d",&bin[i]);
-        if(bin[i]==1){
+        if(bin[i]=='1'){
             dec=dec+base[i];
         }
     }
-    printf("\n\n ");
-    for(int i=0;i<=7;i++){
-        printf("%d",bin[i]);
-    }
-    printf(" --> %d",dec);
+    printf(" %s --> %d",bin,dec);
 }
